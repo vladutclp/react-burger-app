80d00051e4a612f481df422c0416c2b7f1ad95c2
vladutclp <vladut.clapou@gmail.com>
2020-04-26 15:37:57 +0300

    Fixed problem

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button.js';
 
 const orderSummary = (props) => {
 
@@ -15,6 +16,8 @@ const orderSummary = (props) => {
 				{ingredientSummary}
 			</ul>
 			<p>Continue to Checkout?</p>
+			<Button btnType="Danger" clicked={props.cancel}>CANCEL</Button>
+			<Button btnType="Success" clicked={props.continue}>CONTINUE</Button>
 		</Aux>
 	);
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -67,6 +67,11 @@ class BurgerBuilder extends Component{
 	purchaseCancelHandler = () => {
 		this.setState({purchasing: false});
 	}
+
+	purchaseContinueHandler = () => {
+		alert('You continued!');
+	}
+
 	render(){
 		
 		const disabledButton = {...this.state.ingredients};//create an object with booleans based on no of the ingredients object
@@ -77,7 +82,10 @@ class BurgerBuilder extends Component{
 		return(
 			<Aux>
 				<Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-					<OrderSummary ingredients={this.state.ingredients} />
+					<OrderSummary 
+						ingredients={this.state.ingredients} 
+						cancel={this.purchaseCancelHandler}
+						continue={this.purchaseContinueHandler}/>
 				</Modal>
 				<Burger ingredients={this.state.ingredients}/>
 				<BuildControls 
